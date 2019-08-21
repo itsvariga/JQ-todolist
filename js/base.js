fa09912524ec3c5dbeb7b15eaa8bd67b557066f7
黄宇戈 <845047494@qq.com>
2019-08-21 20:18:16 +0800

    Task详情展示功能

--- js/base.js
+++ js/base.js
@@ -5,22 +5,66 @@
 ;(function () {
   'use strict';
   var $from_add_task = $('.add-task')
-    , $delete_task
+    , $task_delete_trigger
+    , $task_detail_trigger
+    , $task_detail = $('.task-detail')
+    , $task_detail_mask = $('.task-detail-mask')
     , task_list = []
     ;
   
   init();
 
   $from_add_task.on('submit', on_add_task_form_submit)
+  $task_detail_mask.on('click', hide_task_detail)
 
   function listen_task_delete() {
-    $delete_task.on('click', function() {
+    $task_delete_trigger.on('click', function() {
       var $this = $(this);
       var $item = $this.parent().parent();
-      var index = $item.data('index')
-      var tmp = confirm('确定删除？')
-      tmp ? delete_task(index) : null
+      var index = $item.data('index');
+      var tmp = confirm('确定删除？');
+      tmp ? delete_task(index) : null;
     })
+  }
+
+  function listen_task_detail() {
+    $task_detail_trigger.on('click', function() {
+      var $this = $(this);
+      var $item = $this.parent().parent();
+      var index = $item.data('index');
+      show_task_detail(index);
+    })
+  }
+
+  function show_task_detail(index) {
+    render_task_detail(index);
+    $task_detail.show();
+    $task_detail_mask.show();
+  }
+
+  function hide_task_detail() {
+    $task_detail.hide();
+    $task_detail_mask.hide();
+  }
+
+  function render_task_detail(index) {
+    if (index === undefined || !task_list[index]) return;
+    var item = task_list[index]
+    var tpl = `
+    <div>
+      <div class="content">${item.content}</div>
+      <div>
+        <div class="desc">
+          <textarea value="${item.desc}"></textarea>
+        </div>
+      </div>
+      <div class="remind">
+        <input type="date">
+      </div>
+    </div>
+    `;
+    $task_detail.html(null);
+    $task_detail.html(tpl);
   }
 
   function add_task(new_task) {
@@ -38,8 +82,10 @@
       var $task = render_task_item(task_list[i], i);
       $task_list.append($task)
     }
-    $delete_task = $('.action.delete')
+    $task_delete_trigger = $('.action.delete')
+    $task_detail_trigger = $('.action.detail')
     listen_task_delete();
+    listen_task_detail();
   }
 
   function render_task_item (data, index) {
@@ -50,7 +96,7 @@
         <span class="task-content">${data.content}</span>
         <span class="fr">
           <span class="action delete"> 删除</span>
-          <span class="action"> 详细</span>
+          <span class="action detail"> 详细</span>
         </span>
       </div>
     `
