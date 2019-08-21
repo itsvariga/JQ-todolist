7ec0839728292b268bb1fd1557b5ddc7955f1fa1
黄宇戈 <845047494@qq.com>
2019-08-21 17:06:55 +0800

    任务删除功能实现

--- js/base.js
+++ js/base.js
@@ -5,32 +5,28 @@
 ;(function () {
   'use strict';
   var $from_add_task = $('.add-task')
+    , $delete_task
     , task_list = []
     ;
   
   init();
 
-  $from_add_task.on('submit', function (e) {
-    var new_task = {}, $input
-    // 禁用默认行为
-    e.preventDefault();
-    // 获取新task的值
-    $input = $(this).find('input[name=content]');
-    new_task.content = $(this).find('input[name=content]').val();
-    // 如果新task为空 则直接返回 否则直接执行
-    if (!new_task.content) return;
-    // 存入新task
-    if (add_task(new_task)) {
-      render_task_list();
-      $input.val(null);
-    }
-  })
+  $from_add_task.on('submit', on_add_task_form_submit)
+
+  function listen_task_delete() {
+    $delete_task.on('click', function() {
+      var $this = $(this);
+      var $item = $this.parent().parent();
+      var index = $item.data('index')
+      var tmp = confirm('确定删除？')
+      tmp ? delete_task(index) : null
+    })
+  }
 
   function add_task(new_task) {
     // 将task推入task_list
     task_list.push(new_task);
-    // 更新localStorage
-    store.set('task_list', task_list);
+    refresh_task_list();
     return true;
   }
 
@@ -39,18 +35,21 @@
     // 先清除$tasks_list的html
     $task_list.html('')
     for (var i = 0; i < task_list.length; i++) {
-      var $task = render_task_tpl(task_list[i]);
+      var $task = render_task_item(task_list[i], i);
       $task_list.append($task)
     }
+    $delete_task = $('.action.delete')
+    listen_task_delete();
   }
 
-  function render_task_tpl (data) {
+  function render_task_item (data, index) {
+    if (!data || index === undefined) return;
     var list_item_tpl = `
-      <div class="task-item">
+      <div class="task-item" data-index="${index}">
         <span><input type="checkbox"></span>
         <span class="task-content">${data.content}</span>
         <span class="fr">
-          <span class="action"> 删除</span>
+          <span class="action delete"> 删除</span>
           <span class="action"> 详细</span>
         </span>
       </div>
@@ -62,6 +61,36 @@
     task_list = store.get('task_list') || [];
     if (task_list.length) {
       render_task_list();
+    }
+  }
+  // 更新localStorage数据并更新tpl
+  function refresh_task_list() {
+    store.set('task_list', task_list);
+    render_task_list();
+  }
+
+  function delete_task(index) {
+    if (index === undefined || !task_list[index]) return;
+    task_list.splice(index, 1)
+    refresh_task_list();
+    if (!task_list.length) {
+      store.remove('task_list');
+      return;
+    }
+  }
+
+  function on_add_task_form_submit(e) {
+    var new_task = {}, $input
+    // 禁用默认行为
+    e.preventDefault();
+    // 获取新task的值
+    $input = $(this).find('input[name=content]');
+    new_task.content = $(this).find('input[name=content]').val();
+    // 如果新task为空 则直接返回 否则直接执行
+    if (!new_task.content) return;
+    // 存入新task
+    if (add_task(new_task)) {
+      $input.val(null);
     }
   }
 })();
